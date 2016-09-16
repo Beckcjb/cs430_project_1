f5b7ac87cdeb4d06104b77e62e15246a350fafac
Beckcjb <cjb456@nau.edu>
2016-09-16 11:21:52 -0700

    Add files via upload
    Changes to ppmwr.c
     - Compilation errors fixed
     - Program runs full but lacks output.

--- ppmrw.c
+++ ppmrw.c
@@ -1,4 +1,4 @@
- // Owner: Charles Beck
+// Owner: Charles Beck
 // Date: 9/7/16
 // Class: CS 430
 // ALLOCATE DATA FOR PIXEL STRUCTURE then just from the 
@@ -7,167 +7,292 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct RGBPixel{										// Data structure for pixel data in image file.
+
+typedef struct RGBPixel{						// Data structure for pixel data in image file.
 	unsigned char r, g, b;										 
 }RGBPixel;
 
-typedef struct Image {											// Structure for image data.
-	int x, y;
+typedef struct Image {							// Structure for image data.
+	int w, h;
 	RGBPixel *data;
 } Image;
 
-#define creator "CJB456"
-#define rgb_color 255
+#define rgb_color 255							// We are not using ppm files that obtain an alpha channel 
+												// 		so we can define rgb color depth as 255
 
-int type; 														// Allocate memory for responses.
+char* type; 									// Allocate memory for responses.
 int width;
-int height;														// Array for ascii data and binary data.
+int height;										
 char* fileNameIn;	
-char* fileNameOut;				
-																// File name value.
-Image *img;
+char* fileNameOut;	
+char* buffer[24];
+int fileHeader[2];			
+FILE* fi;
+FILE* fo;										// File name value.
+Image *imaged;									// pointer to Image data
 
-																			// void write_P6(char *fileName, Image *img)
-void write_P6(Image *img)
+												// void write_P6(char *fileName, Image *img)
+void write_P6(char* fileNameOut, Image *imaged)
 {
-	width = img->y;
-	height = img->x;
-	int biit;
-    FILE *fh = fopen("output.ppm", "w");
-														//open file for outpu
-    if (!fh) {
-         printf("Unable to open file '%s'\n", fileNameOut);
+	width = imaged->w;
+	height = imaged->h;
+    FILE *fo = fopen(fileNameOut, "wb");
+												//open file for outpu
+    if (!fo) {
+         fprintf(stderr, "Unable to open file '%s'\n", fileNameOut);
          exit(1);
     }
-																			//write the header file
-																			//image format
-    fprintf(fh, "P3\n");
-	
+												//write the header file
+												//image format
+    fprintf(fo, "P3\n");
+	printf("P3");
+												//image size
+    fprintf(fo, "%d %d\n",imaged->w,imaged->h);
+	printf("%d %d\n",imaged->w,imaged->h);
 
-																			//comments
-    fprintf(fh, "# Created by %s\n",creator);
-	
-																			//image size
-    fprintf(fh, "%d %d\n",img->x,img->y);
-
-																			// rgb alpha
-    fprintf(fh, "%d\n",rgb_color);
+												// rgb alpha
+    fprintf(fo, "%d\n",rgb_color);
+	printf("%d....\n",rgb_color);
 
     // pixel data
-    fwrite(img->data, 3 * img->x, img->y, fh);
+    fwrite(imaged->data, 3 * imaged->w, imaged->h, fo);
 	
-    fclose(fh);
+    fclose(fo);
+	printf("File store complete");
 }
 
-void write_P3(Image *img)
+void write_P3(char* fileNameOut, Image *imaged)
 {
-	width = img->y;
-	height = img->x;
+	width = imaged->w;
+	height = imaged->h;
 
-    FILE *fh = fopen("output.ppm", "w");
-														//open file for output
-    if (!fh) {
-         printf("Unable to open file '%s'\n", fileNameOut);
-         exit(1);
-    }
-																			//write the header file
-																			//image format
-    fprintf(fh, "P6\n");
-																			// add comments and creator information
-    fprintf(fh, "# Created by %s\n",creator);
+    FILE *fo = fopen(fileNameOut, "w");				//open file for output
+												
+													//write the header file
+													//image format
+    fprintf(fo, "P3\n");
+	printf("P3");
+													//image size
+    fprintf(fo, "%d %d\n",imaged->w,imaged->h);
+	printf("%d %d\n",imaged->w,imaged->h);
+
+													// rgb alpha
+    fprintf(fo, "%d\n",rgb_color);
+	printf("%d....\n",rgb_color);
+													// write pixel data
+    fwrite(imaged->data, 3 * imaged->w, imaged->h, fo);
 	
-																			//image size
-    fprintf(fh, "%d %d\n",img->x,img->y);
-
-																			// rgb alpha
-    fprintf(fh, "%d\n",rgb_color);
-
-																			// write pixel data
-    fwrite(img->data, 3 * img->x, img->y, fh);
-	
-    fclose(fh);
+    fclose(fo);
+	printf("File store complete");
 }
 
-void read_file(){	
-	printf("Enter in the name of file with extention you wish to store (#input.ppm): ");	// Ask what file they want converted.
-	scanf ("%s", fileNameIn);										// Scan in answer.
-	if(strstr(fileNameIn,  ".ppm" ) == NULL)
-			printf("Error: usage ppmrw.c #input.ppm output.ppm\n");
-	int i, j, count, comments, d ,endOfHead, headerLength;
+void readStoreASCII(char* fileNameIn, char* fileNameOut, char* type){	
+	printf("What happened here tooa?\n");
+		
+	int i, j, count, comments, d ,endOfHead, headerLength ;
+	char reader;
 	
-	FILE* fh = fopen(fileNameIn, "r");
-	endOfHead = getc(fh);
+
+	width = imaged->w;
+	height = imaged->h;
 	
-    while (getc(fh) != '\n') {
-         endOfHead = getc(fh);
-		 count++;												
-		 if(getc(fh) == '\n'){
-																// Break loop if the header reaches a new line
-		 }														// PPM headers are formatted to have a new line at the end of the header
-    }
-	ungetc(endOfHead, fh);
+	FILE* fi = fopen(fileNameIn, "r");
 	
-	char fileHeader[count];
-	if (!fh) {													// Check if file exists
-		printf("File not able to open, may not exist.\n", fileNameIn);
+	fread(imaged->data, width, height, fi);
+	int g = fgetc(fi);
+	for(i = 0 ; i<2; i++){
+		g = fgetc(fi);
+		fileHeader[i] = g;
+		printf("%c\n", g);
+		
+	}
+	if (fileHeader[0] != 'P' || fileHeader[1] != '3'){			// Check if file is P3 or P6.
+		fprintf(stderr, " ascii: Error: Usage, P3 and P6  files only\n");
+		exit(1);
+	}
+	
+
+
+	
+	if (!fi) {													// Check if file existsn 
+		fprintf(stderr, "File not able to open, may not exist.\n", fileNameIn);
 		exit(1);
 		}
-	for (i = 0; i < count; i++){									// File type p3 or p6
-		d = fgetc(fh);
-		fileHeader[i] = d;
-	}	
-	if (fileHeader[2] == 3){
-		write_P6(img);
-	}
-	if (fileHeader[2] == 6){
-		write_P3(img);
-	}
-
 																//check for comments
-     comments = getc(fh);
+     comments = getc(fi);
     while (comments == '#') {
-    while (getc(fh) != '\n') ;
-         comments = getc(fh);
+    while (getc(fi) != '\n') ;
+         comments = getc(fi);
     }
-																//check for comments
-     d = getc(fh);
+																//check for whitespaces
+     d = getc(fi);
     while (d == ' ') {
-    while (getc(fh) != '\n') ;
-         d = getc(fh);
+    while (getc(fi) != '\n') ;
+         d = getc(fi);
     }
-	if (fileHeader[0] != 'P' || fileHeader[1] != '3' ||fileHeader[1] != '6'){				// Check if file is P3 or P6.
-		printf("Error: Usage, P3 and P6  files only\n");
-		exit(1);
-	}
+
 	
-	//read image size information
-    if (fscanf(fh, "%d %d", &img->x, &img->y) != 2) {
-        printf("Invalid image size (error loading '%s')\n", fileNameIn);
+																//read image size information
+    if (fscanf(fi, "%d %d", &width, &height) != 2) {
+        fprintf(stderr, "Invalid image size (error loading '%s')\n", fileNameIn);
          exit(1);
     }
 	
-	while (fgetc(fh) != '\n') ;
-																			//memory allocation for pixel data
-    img->data = (RGBPixel*)malloc(img->x * img->y * sizeof(RGBPixel));
+	while (fgetc(fi) != '\n') ;
+																//memory allocation for pixel data
+    imaged->data = (RGBPixel*)malloc(imaged->w * imaged->h * sizeof(RGBPixel));
+    if (!imaged) {
+         fprintf(stderr, "Unable to allocate memory\n");
+         exit(1);
+    }
+
+																//read pixel data from file
+	int pix;													// Read file data
+	for(i = 0 ; i < height; i++ ){
+		for(j = 0; j < width; j ++){
+			fscanf(fi, "%d", &pix);
+			imaged->data[i*width*3+3*j] = pix;
+			
+			fscanf(fi, "%d", &pix);
+			imaged->data[i*width*3+3*j+1] = pix;
+			
+			fscanf(fi, "%d", &pix);
+			imaged->data[i*width*3+3*j+2] = pix;
+		}
+	}
 	
-    if (!img) {
-         printf("Unable to allocate memory\n");
+	fclose(fi);	// Close file
+	printf("What happened here too?\n");
+	if (type == "3"){
+		write_P6(fileNameOut, imaged);   	// if the number selected as output is a 3 go to write P3
+	}
+	if (type == "6"){
+		write_P3(fileNameOut, imaged);		// if the number selected as output is a 6 go to write P6
+	}
+	
+}
+
+void readStoreRAW(char* fileNameIn, char* fileNameOut, char* type){	
+	printf("What happened here too?\n");
+	int i, j, count, comments, d ,endOfHead, headerLength, n;
+	char reader;
+	
+	width = imaged->w;
+	height = imaged->h;
+	
+	FILE* fi = fopen(fileNameIn, "rb");
+	
+	fread(imaged->data, width, height, fi);
+	for (i = 0; i < 2; i++){									// File type p3 or p6
+		d = fgetc(fi);
+		fileHeader[i] = d;
+		}	
+
+	if (fileHeader[1] != 6){
+		fprintf(stderr, "Error: File must be P3 or P6\n");
+	exit(1);}
+	
+	if (!fi) {													// Check if file exists
+		fprintf(stderr, "raw: File not able to open, may not exist.\n", fileNameIn);
+		exit(1);
+		}
+																//check for comments
+     comments = getc(fi);
+    while (comments == '#') {
+    while (getc(fi) != '\n'){
+		comments = getc(fi);}
+    }
+																//check for whitespaces
+     d = getc(fi);
+    while (d == ' ') {
+    while (getc(fi) != '\n'){
+		d = getc(fi);}
+    }
+	if (fileHeader[1] != '3' ||fileHeader[1] != '6'){						// Check if file is P3 or P6.
+		fprintf(stderr, "raw: Error: Usage, P3 and P6  files only\n");
+		exit(1);
+	}
+	
+																			//read image size information
+    if (fscanf(fi, "%d %d", &width, &height) != 2) {
+        fprintf(stderr, "raw: Invalid image size (error loading '%s')\n", fileNameIn);
+         exit(1);
+    }
+	
+	while (fgetc(fi) != '\n');
+																			//memory allocation for pixel data
+    imaged->data = (RGBPixel*)malloc(imaged->w * imaged->h * sizeof(RGBPixel));
+	
+    if (!imaged) {
+         fprintf(stderr, "raw: Unable to allocate memory\n");
          exit(1);
     }
 
 																			//read pixel data from file
-    if (fread(img->data, 3 * img->x, img->y, fh) != img->y) {
-         printf("Error: Issue reading image '%s'\n", fileNameIn);
+    if (fread(imaged->data, 3 * imaged->w, imaged->h, fi) != imaged->h) {
+         fprintf(stderr, "raw: Error: Issue reading image '%s'\n", fileNameIn);
          exit(1);
 	}
 	
-	fclose(fh);
+	fclose(fi);	// Close file
+	printf("What happened here too?\n");
+	if (type == "3"){
+		write_P6(fileNameOut, imaged);   	// if the number selected as output is a 3 go to write P3
+	}
+	if (type == "6"){
+		write_P3(fileNameOut, imaged);		// if the number selected as output is a 6 go to write P6
+	}
+
 }
 
 
 
 
 int main(int argc, char *argv[]){
-	printf("p3 and p6 converter:\n");	
-	read_file();
+	if (argc != 4) {
+		fprintf(stderr, "Error: Usage ppmrw # input.ppm output.ppm");
+		exit(1);
 	}
+	int d;
+	fileNameOut = argv[3];
+	fileNameIn = argv[2];
+	type = argv[1];
+	char* program = argv[0];
+	if(strstr(program, "ppmrw") == NULL){							// Check if argument is valid
+		fprintf(stderr, "Error: main Usage ppmrw 3(6) input.ppm output.ppm\n");
+		exit(1);
+	}
+	if(strstr(fileNameIn,  ".ppm" ) == NULL){						// Check if file is in .ppm format
+			fprintf(stderr, "Error: main usage ppmrw 3(6) input.ppm output.ppm\n");
+			exit(1);
+	}
+	if(strstr(fileNameOut,  ".ppm" ) == NULL){						// Check if file is in .ppm format
+			fprintf(stderr, "Error: main usage ppmrw 3(6) input.ppm output.ppm\n");
+			exit(1);
+	}
+	FILE* fi = fopen(fileNameIn, "r");
+	if (!fi) {													// Check if file exists
+		fprintf(stderr, "main: File not able to open, %s may not exist.\n ", fileNameIn);
+		exit(1);
+		}
+	for (int i = 0; i <2 ; i++){									// get File type p3 or p6
+		d = fgetc(fi);
+		fileHeader[i] = d;
+		printf("%c", d);
+		if (fileHeader[1] == 3){
+			fclose(fi);		
+			printf("WTF IS GOING ON!");				// If input file is p3 go to store data in ascii
+			readStoreASCII(fileNameIn, fileNameOut, type);
+			
+		}
+		if (fileHeader[1] == 6){		// If input file is p6 go to store data in RAW
+			fclose(fi);
+			readStoreRAW(fileNameIn, fileNameOut, type);
+		}	
+		
+		}
+		
+		
+	fclose(fi);
+
+}
